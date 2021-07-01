49a6e3806e2d3d8706d3ff275968052fabd178dd
M. Smith <rubys_place@hotmail.com>
2021-06-30 19:54:33 -0700

    init commit

---
+++ AdvancedInterviewProject/AdvancedInterviewProject.swift
@@ -0,0 +1,21 @@
+//
+//  AdvancedInterviewProject.swift
+//  AdvancedInterviewProject
+//
+//  Created by Matt M Smith on 6/30/21.
+//
+
+import SwiftUI
+
+struct AdvancedInterviewProject: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdvancedInterviewProject()
+    }
+}
---
+++ AdvancedInterviewProject/AdvancedInterviewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedInterviewProjectApp.swift
+//  AdvancedInterviewProject
+//
+//  Created by Matt M Smith on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedInterviewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AdvancedInterviewProject()
+        }
+    }
+}
