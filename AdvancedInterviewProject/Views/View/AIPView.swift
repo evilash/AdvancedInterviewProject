a687bfae1b59ed8c444beab9a1d66bfa8f132369
M. Smith <rubys_place@hotmail.com>
2021-08-10 16:13:14 -0700

    Created a wrapper for quicker view creation.

---
+++ AdvancedInterviewProject/Views/View/AIPView.swift
@@ -0,0 +1,28 @@
+//
+//  AIPView.swift
+//  AdvancedInterviewProject
+//
+//  Created by Matt M Smith on 8/10/21.
+//
+
+import SwiftUI
+
+struct AIPView<Content: View, Destination>: View where Destination : View {
+    let content: Content
+    let destination: Destination
+    let label: String
+    var count: Int
+    
+    init(navigation: Destination, navLabel: String, count: Int = 5, @ViewBuilder content: () -> Content) {
+        destination = navigation
+        label = navLabel
+        self.count = count
+        self.content = content()
+    }
+    
+    var body: some View {
+        content
+        
+        AIPNavigation(label: label, destination: destination, count: count)
+    }
+}
